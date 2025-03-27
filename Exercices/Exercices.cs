b8735eb308785a004fe120ccdaa960f09c14d06e
Rani BELLIL <rani.bellil@wisecom.fr>
2025-03-27 21:28:02 +0100

    tests AffichePrix validés

--- Exercices/Exercices.cs
+++ Exercices/Exercices.cs
@@ -1,6 +1,30 @@
-﻿namespace exercices;
+﻿
 
-public class Class1
+namespace exercices;
+
+public class Exercices
 {
+    public string AffichePrix(int nbre, decimal pU, int taxe)
+    {
+        decimal prixTTC = CalculPrix(nbre, pU, taxe);
+        return $"{nbre} articles à {pU} et taxe à {taxe} % - {Math.Round(prixTTC, 2)}";
 
+        static decimal CalculPrix(int nbre, decimal pU, int taxe)
+        {
+            decimal prixHT = nbre * pU;
+            decimal prixTTC = prixHT + (prixHT * taxe / 100);
+
+            if (prixHT >= 1000 && prixHT < 5000)
+            {
+                prixTTC -= (prixTTC * 3 / 100);
+            }
+
+            if (prixHT >= 5000)
+            {
+                prixTTC -= (prixTTC * 5 / 100);
+            }
+
+            return prixTTC;
+        }
+    }
 }
--- ExercicesTests/ExercicesTests.cs
+++ ExercicesTests/ExercicesTests.cs
@@ -1,10 +1,44 @@
-﻿namespace ExercicesTests;
+﻿
+using exercices;
+using FluentAssertions;
+
+namespace ExercicesTests;
 
 public class ExercicesTests
 {
-    [Fact]
-    public void Test1()
+     
+    [Theory]
+    [InlineData(3, 1.21, 0, 3.63)]
+    [InlineData(3, 1.21, 5, 3.81)]
+    [InlineData(3, 1.21, 20, 4.36)]
+    public void TestPrixSansRemise(int nbre, decimal pU, int taxe , decimal expected)
     {
+       // Arrange
+       var ex = new Exercices();
+         // Act
+        string result  = ex.AffichePrix(nbre, pU,taxe);
 
+        // Assert
+        result.Should().Be($"{nbre} articles à {pU} et taxe à {taxe} % - {expected}");
+
+        
     }
+
+    [Theory]
+    [InlineData(5, 345, 10, 1840.58)]
+    [InlineData(5, 1299, 10, 6787.28)]
+    public void TestPrixAvecRemise(int nbre, decimal pU, int taxe , decimal expected)
+    {
+       // Arrange
+       var ex = new Exercices();
+         // Act
+        string result  = ex.AffichePrix(nbre, pU,taxe);
+
+        // Assert
+        result.Should().Be($"{nbre} articles à {pU} et taxe à {taxe} % - {expected}");
+
+        
+    }
+
+
 }
